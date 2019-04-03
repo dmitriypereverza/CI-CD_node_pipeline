393e0e46876652b0bbf9d0d5210f445392ced058
dmitrijpereverza <disel920@gmail.com>
2019-04-03 19:29:29 +0300

    init commit

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express, { Request } from 'express';
+import childProcess from 'child_process';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (req: Request, res) => {
+  res.send(JSON.stringify(req.query))
+});
+app.post('/webhooks', (req: Request, res) => {
+  const branch = req.body.ref;
+  deploy(res);
+  // res.send(JSON.stringify(req.body))
+});
+
+
+function deploy(res){
+  childProcess.exec('ls -la', function(err, stdout, stderr){
+    if (err) {
+      console.error(err);
+      return res.send(500);
+    }
+    res.send(stdout);
+  });
+}
+
+
+app.listen(3000, () => console.log('CI service listening on port 3000!'));
