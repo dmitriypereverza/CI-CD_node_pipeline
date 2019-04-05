5193f3ca98e52a378e27579a2e43a19b5361eb4d
dmitrijpereverza <disel920@gmail.com>
2019-04-05 21:52:48 +0300

    Заготовка под деплой на продакшн сервер

--- src/handlers/deploy.ts
+++ src/handlers/deploy.ts
@@ -1,20 +1,52 @@
 import { Request, Response } from "express";
 import { Error } from "nodegit";
+import NodeSsh from "node-ssh";
 const Git = require("nodegit");
 
+const ssh = new NodeSsh();
+
 export default function deploy({ body: { project, git, privateKey } }: Request, res: Response){
-  Git.Clone(git, "tmp")
+  getProjectConfig(git, res)
+    .then(config => {
+      console.log(config);
+
+      ssh.connect({
+        host: '194.87.93.250',
+        username: 'root',
+        port: 22,
+        password: 'pbb2c7do',
+        tryKeyboard: true,
+        onKeyboardInteractive: (name, instructions, instructionsLang, prompts, finish) => {
+          if (prompts.length > 0 && prompts[0].prompt.toLowerCase().includes('password')) {
+            finish(['pbb2c7do'])
+          }
+        }
+      }).then(function() {
+        ssh.execCommand('git status', { cwd: '/var/www/parsers' }).then(function(result) {
+          console.log('STDOUT: ' + result.stdout);
+          if (result.stderr) {
+            console.log('STDERR: ' + result.stderr);
+          }
+        });
+      }).catch(function (err) {
+        console.log(err);
+      });
+    });
+}
+
+function getProjectConfig (git, res: Response) {
+  return Git.Clone(git, "tmp")
     .then(repo => repo.getMasterCommit())
-    .then(commit => commit.getEntry("vue.config.jsp"))
+    .then(commit => commit.getEntry("vue.config.js"))
     .then(entry => entry.getBlob().then(blob => {
         blob.entry = entry;
         return blob;
       })
     )
-    .then(function(blob) {
-      res.send(String(blob));
+    .then(function (blob) {
+      return String(blob);
     })
-    .catch(function(err: Error) {
+    .catch(function (err: Error) {
       console.log(err.message);
       res.send(JSON.stringify({
         success: false,
