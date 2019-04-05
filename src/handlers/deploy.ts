9ea5ffa86f624386414f75a27732a1413b5a0f0a
dmitrijpereverza <disel920@gmail.com>
2019-04-05 22:31:10 +0300

    Заготовка под деплой на продакшн сервер

--- src/handlers/deploy.ts
+++ src/handlers/deploy.ts
@@ -5,34 +5,32 @@ const Git = require("nodegit");
 
 const ssh = new NodeSsh();
 
-export default function deploy({ body: { project, git, privateKey } }: Request, res: Response){
-  getProjectConfig(git, res)
-    .then(config => {
-      console.log(config);
+const deploy = async function deploy({ body: { project, git, privateKey } }: Request, res: Response){
+  const config = await getProjectConfig(git, res);
+  console.log(config);
 
-      ssh.connect({
-        host: '194.87.93.250',
-        username: 'root',
-        port: 22,
-        password: 'pbb2c7do',
-        tryKeyboard: true,
-        onKeyboardInteractive: (name, instructions, instructionsLang, prompts, finish) => {
-          if (prompts.length > 0 && prompts[0].prompt.toLowerCase().includes('password')) {
-            finish(['pbb2c7do'])
-          }
-        }
-      }).then(function() {
-        ssh.execCommand('git status', { cwd: '/var/www/parsers' }).then(function(result) {
-          console.log('STDOUT: ' + result.stdout);
-          if (result.stderr) {
-            console.log('STDERR: ' + result.stderr);
-          }
-        });
-      }).catch(function (err) {
-        console.log(err);
-      });
+  ssh.connect({
+    host: '194.87.93.250',
+    username: 'root',
+    port: 22,
+    password: 'pbb2c7do',
+    tryKeyboard: true,
+    onKeyboardInteractive: (name, instructions, instructionsLang, prompts, finish) => {
+      if (prompts.length > 0 && prompts[0].prompt.toLowerCase().includes('password')) {
+        finish(['pbb2c7do'])
+      }
+    }
+  }).then(() => {
+    ssh.execCommand('git status', { cwd: '/var/www/parsers' }).then(function(result) {
+      console.log('STDOUT: ' + result.stdout);
+      if (result.stderr) {
+        console.log('STDERR: ' + result.stderr);
+      }
     });
-}
+  }).catch(function (err) {
+    console.log(err);
+  });
+};
 
 function getProjectConfig (git, res: Response) {
   return Git.Clone(git, "tmp")
@@ -54,3 +52,6 @@ function getProjectConfig (git, res: Response) {
       }));
     });
 }
+
+
+export default deploy;
