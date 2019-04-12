0efa289760251ee42c7e91da3a0b27fb824fedc8
dmitrijpereverza <disel920@gmail.com>
2019-04-12 21:13:08 +0300

    Добавлена обработка ошибок в процессе деплоя

--- src/requestHandlers/deploy.ts
+++ src/requestHandlers/deploy.ts
@@ -9,26 +9,31 @@ import { buildProjectConfig } from "../config/projectConfigBuilder";
 import db from "../Storage";
 
 const Git = require("nodegit");
-
 const ssh = new NodeSsh();
 
 const deploy = async function deploy({ body: { ref, repository: { clone_url } } }: Request, res: Response){
-  // TODO по гиту определить проект
   const project = await db.findOne({ git: clone_url });
+  if (!project) {
+    emitter.emit('log.error', `Не зарегистрирован проект для репозитория ${clone_url}`);
+    res.send();
+    return;
+  }
 
-
-  let config = await getProjectConfig(clone_url, res);
-
-  // TODO проверить объект
-
-
+  let config = await getProjectConfig(project, res);
+  if (!config) {
+    emitter.emit('notify.user', project.notificationEmails,
+      `Не удалось получить конфиг проекта. Проверте настройки проекта.`
+    );
+    res.send();
+    return;
+  }
 
   const validationResponse = validate(config, require("../jsonSchemas/client_config.json"));
   if (!validationResponse.isValid) {
-    emitter.emit('notify.user', 'disel920@gmail.com',
+    emitter.emit('notify.user', project.notificationEmails,
       `Получен невалидный конфиг файл проекта. Ошибки: ${JSON.stringify(validationResponse.errors)}`
     );
-    res.send(`Project config not valid. Errors: ${JSON.stringify(validationResponse.errors)}`);
+    res.send();
     return;
   }
 
@@ -36,61 +41,66 @@ const deploy = async function deploy({ body: { ref, repository: { clone_url } } 
   try {
     builderConfig = buildProjectConfig(config, project.privateParam);
   } catch (e) {
-    emitter.emit('notify.user', 'disel920@gmail.com', e.message);
-    res.send(e.message);
+    emitter.emit('notify.user', project.notificationEmails, `Произошли ошибки при сборке файла конфигурации: ${e.message}`);
+    res.send();
     return;
   }
 
-  // TODO тут должны определить в какую ветку произвести деплой
   const target = builderConfig.targets.find(target => `refs/heads/${target.branch}` === ref);
+  if (!target) {
+    res.send();
+    return;
+  }
 
-  ssh.connect({
-    host: target.deploy.ssh.host,
-    username: target.deploy.ssh.username,
-    port: target.deploy.ssh.port,
-    password: target.deploy.ssh.pass,
+  connectToServer(target.deploy.ssh)
+    .then(() => {
+      ssh.execCommand(target.deploy.command, { cwd: target.deploy.ssh.cwd })
+        .then(function(result) {
+          emitter.emit('notify.user', project.notificationEmails,
+            'STDOUT: ' + result.stdout + "\nSTDERR: " + result.stderr);
+          res.send();
+        });
+      }).catch(function (err) {
+        emitter.emit('notify.user', project.notificationEmails, `Проблема подключения к удаленному серверу: ${err}`);
+        res.send();
+      });
+};
+
+function connectToServer (sshConfig) {
+  const { host, username, port, pass } = sshConfig;
+  return ssh.connect({
+    host: host,
+    username: username,
+    port: port,
+    password: pass,
     tryKeyboard: true,
     onKeyboardInteractive: (name, instructions, instructionsLang, prompts, finish) => {
       if (prompts.length > 0 && prompts[0].prompt.toLowerCase().includes('password')) {
-        finish([target.deploy.ssh.pass])
+        finish([pass])
       }
     }
-  }).then(() => {
-    ssh.execCommand(target.deploy.command, { cwd: target.deploy.ssh.cwd })
-      .then(function(result) {
-        emitter.emit('notify.user', target.notification.email,
-          'STDOUT: ' + result.stdout + "\nSTDERR: " + result.stderr);
-        res.send(JSON.stringify({ success: true }));
-      });
-    }).catch(function (err) {
-      emitter.emit('notify.user', target.notification.email, 'Проблема подключения к удаленному серверу: ' + err);
-      res.send(JSON.stringify({
-        success: false,
-        error: 'Проблема подключения к удаленному серверу: ' + err,
-      }));
-    });
-};
+  })
+}
 
-function getProjectConfig (git, res: Response) {
-  rimraf.sync("tmp");
-  return Git.Clone(git, "tmp")
+function getProjectConfig (project, res: Response) {
+  return Git.Clone(project.git, "tmp")
     .then(repo => repo.getMasterCommit())
     .then(commit => commit.getEntry("project.conf.json"))
-    .then(entry => entry.getBlob().then(blob => {
+    .then(entry => entry.getBlob()
+      .then(blob => {
         blob.entry = entry;
         return blob;
       })
     )
-    .then(function (blob) {
-      return String(blob);
-    }).then(json => {
+    .then(blob => String(blob))
+    .then(json => {
+      rimraf.sync("tmp");
       return JSON.parse(json)
     })
     .catch(function (err: Error) {
-      res.send(JSON.stringify({
-        success: false,
-        error: err.message,
-      }));
+      emitter.emit(project.notificationEmails, `Произошли ошибка при скачивании репозитория: [${err.message}]`);
+      rimraf.sync("tmp");
+      res.send();
     })
 }
 
